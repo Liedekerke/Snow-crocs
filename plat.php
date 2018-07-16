7a2dea2c8b55fe75e78dd683a0946a48a00d5094
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-16 16:14:20 +0200

    php suite

--- plat.php
+++ plat.php
@@ -45,7 +45,7 @@
   <script>
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
-        navigator.serviceWorker.register('assets/JS/service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
           // Registration was successful
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
