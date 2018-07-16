4559dab0f786d383232713be95971ef48aee660c
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-16 21:08:46 +0200

    style..

--- index.php
+++ index.php
@@ -2,7 +2,15 @@
 <html>
 
 <head>
-<?php include('assets/includes/head.php'); ?>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+  <link rel="manifest" href="assets/manifest.json" />
+  <meta name="theme-color" content="#317EFB" />
+  <meta name="Description" content="oui" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <title>Va & Vins</title>
 </head>
 
