a1d29c9f0296035f888697afa73b4c19f84d5fdc
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-16 16:38:25 +0200

    php fin git

--- assets/includes/photos.php
+++ assets/includes/photos.php
@@ -2,10 +2,7 @@
 
   <nav aria-label="photos">
     <ul class="pagination">
-      <li>
-        <a href="plat.php">
-          <<< </a>
-      </li>
+      <li><a href="plat.php"> <<< </a></li>
       <li><a href="photo.php"> 1 </a></li>
       <li><a href="plat.php"> 2 </a></li>
       <li><a href="dessert.php"> 3 </a></li>
