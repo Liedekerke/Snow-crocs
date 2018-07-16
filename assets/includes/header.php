915a038a66db1693d4f06cd198775253a913d67c
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-16 14:51:37 +0200

    php

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -5,11 +5,11 @@
     <img...>
       <nav class="navbar navbar-inverse" id="FiveFingers">
         <ul class="nav navbar-nav" id="FingersMoves">
-          <li id="FingerFive"><a href="index.html"> Accueil</a></li>
-          <li id="FingerOne"><a href="carte.html"> Carte</a></li>
-          <li id="FingerTwo"><a href="photo.html"> Photos</a></li>
-          <li id="FingerThree"><a href="restaurant.html">Restaurants</a></li>
-          <li id="FingerFour"><a href="contact.html">Contact</a></li>
+          <li id="FingerFive"><a href="index.php"> Accueil</a></li>
+          <li id="FingerOne"><a href="carte.php"> Carte</a></li>
+          <li id="FingerTwo"><a href="photo.php"> Photos</a></li>
+          <li id="FingerThree"><a href="restaurant.php">Restaurants</a></li>
+          <li id="FingerFour"><a href="contact.php">Contact</a></li>
         </ul>
   </div>
   </nav>
