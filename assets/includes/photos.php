af7a56ec8d46f1609259499e3f95d19286fcd382
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-16 16:40:43 +0200

    php fin git

--- assets/includes/photos.php
+++ assets/includes/photos.php
@@ -1,13 +1,13 @@
 <section class="devienFou">
-
   <nav aria-label="photos">
     <ul class="pagination">
+
       <li><a href="plat.php"> <<< </a></li>
       <li><a href="photo.php"> 1 </a></li>
       <li><a href="plat.php"> 2 </a></li>
       <li><a href="dessert.php"> 3 </a></li>
       <li><a href="photo.php"> >>> </a></li>
+
     </ul>
   </nav>
-
 </section>
