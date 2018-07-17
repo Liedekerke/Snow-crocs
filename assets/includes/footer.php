6f36b2ca1a0d5d55bbd89966c902a21dce07d647
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-17 16:26:06 +0200

    suit

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,4 +1,4 @@
 
   <div class="main-footer">
-    <p> <span class="glyphicon glyphicon-copyright-mark"></span> Copyright- Nicolas de Liedekerke </p>
+    <p id="copiriri"> <span class="glyphicon glyphicon-copyright-mark"></span> Copyright- Nicolas de Liedekerke </p>
   </div>
