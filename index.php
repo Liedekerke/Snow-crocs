2ba6e5acb4383667b5db744e84f71dcacfaf78de
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-17 15:22:45 +0200

    suit

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 </header>
 
 <main>
-<div class="jarallax jarallax-home" style="background-image: url('/assets/images/splashart3.jpg'); background-repeat: no-repeat;position: absolute;top: 0px; left:0px; width:100%; height: 100%; overflow: hidden; pointer-events:none; z-index: -100;">
+<!-- <div class="jarallax jarallax-home" style="background-image: url('/assets/images/splashart3.jpg'); background-repeat: no-repeat;position: absolute;top: 0px; left:0px; width:100%; height: 100%; overflow: hidden; pointer-events:none; z-index: -100;"> -->
 
 </div>
 </main>
