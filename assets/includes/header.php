ce65f917f4dfec39ffdfc4a23769856b673ee731
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-17 15:58:28 +0200

    suit

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -14,7 +14,9 @@
   </div>
   </nav>
 </header> -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-sc-dark">
+
+<!-- veqaaaaaaaaevvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv -->
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-sc-dark">
   <div class="container nav-container">
     <a class="navbar-brand" href="index.php">
       <h1 style="height:26px;"> Ginette </h1>
@@ -42,5 +44,37 @@
       </li>
     </ul>
   </div>
+  </div>
+</nav> -->
+<!-- OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-sc-dark">
+  <div class="container nav-container">
+    <a class="navbar-brand" href="index.php">
+      <h2 style="height:26px;"> Snow Crows </h2>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php"> Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="carte.php"> Carte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="photo.php"> Photos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="restaurant.php">Restaurants</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
