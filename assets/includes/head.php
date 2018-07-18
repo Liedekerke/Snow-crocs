00638a3e30c6ab1cc61299858e318381755cbe5b
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-18 16:00:31 +0200

    test

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  
+
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,6 +11,5 @@
 <link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
 <link rel="manifest" href="/assets/manifest.json" />
 <link rel="stylesheet" href="/assets/css/jarallax.css">
-<script src="/assets/js/jarallax.js"></script>
 <meta name="theme-color" content="#317EFB"/>
 <meta name="Description" content="oui"/>
