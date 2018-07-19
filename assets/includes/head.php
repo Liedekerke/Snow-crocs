3232f017e5204521c1cc8919ecd776da06c61cea
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-19 14:40:57 +0200

    test

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -7,9 +7,19 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700" rel="stylesheet">
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="/assets/css/style.css" />
-<link rel="manifest" href="/assets/manifest.json" />
+
 <link rel="stylesheet" href="/assets/css/jarallax.css">
+<script src="assets/js/jarallax-0.2.1.js"></script>
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+<!-- <script src="assets/js/jarallax-element.js"></script> -->
+
+<link rel="stylesheet" href="assets/css/main.css">
+
+
+
+<link rel="manifest" href="/assets/manifest.json" />
+
 <meta name="theme-color" content="#317EFB"/>
 <meta name="Description" content="oui"/>
