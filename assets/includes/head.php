69a9c1835985ce22830b07b0bb3991d19b74541f
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-19 15:04:02 +0200

    test

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -11,11 +11,15 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
 <link rel="stylesheet" href="/assets/css/jarallax.css">
-<script src="assets/js/jarallax-0.2.1.js"></script>
+<script src="assets/js/jarallax.js"></script>
+<script src="assets/js/jarallax-video.js"></script>
+<script src="assets/js/jarallax-element.js"></script>
+
 <script src="assets/js/jquery-3.3.1.min.js"></script>
-<!-- <script src="assets/js/jarallax-element.js"></script> -->
 
 <link rel="stylesheet" href="assets/css/main.css">
+<!-- <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script> -->
 
 
 
