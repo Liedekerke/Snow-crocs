e8d0952d3057d44fe8cd4695c9e3eefe17593e13
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-19 12:21:56 +0200

    test

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -4,7 +4,7 @@
   </div>
 </footer>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 
@@ -24,8 +24,7 @@
   }
 </script>
 
-<script type="text/javascript" src="assets/js/jarallax-0.2.1.js"></script>
-<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+
 <!-- <script>
 init = function(){
       var jarallax = new Jarallax();
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include('assets/includes/head.php'); ?>
 
+<script type="text/javascript" src="assets/js/jarallax-0.2.1.js"></script>
+<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+
   <title>oui</title>
 </head>
 
@@ -17,54 +20,52 @@
 <!-- BG -->
 <div class="jarallax jarallax-home" style="background-image: url('/assets/images/splashart3.jpg');">
 
-<div id="jarallax-container-0" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; z-index: -100;">
-  <div style="background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url(&quot;/assets/images/splashart3.jpg&quot;); position: fixed; top: 0px; left: 0px; width: 674px; height: 615px; overflow: hidden; pointer-events: none; margin-top: 0px; transform: translate3d(0px, 28px, 0px);">
-  </div>
-</div>
 </div>
 
-<!-- CONTAINER DARK -->
 <div class="container-fluid container-dark" style="border-bottom: 24px solid #181818;padding-bottom:0px;">
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="photo.php">
-        <img src="assets/images/imagetest.jpg" class="img-fluid img-max img-home-button">
+        <a href="carte.php">
+        <img src="assets/images/image4.png" class="img-fluid img-max img-home-button">
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
         <a href="photo.php">
-        <img src="assets/images/image5.jpg" class="img-fluid img-max img-home-button">
+        <img src="assets/images/image4.png" class="img-fluid img-max img-home-button">
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="photo.php">
-        <img src="assets/images/image4.jpg" class="img-fluid img-max img-home-button">
+        <a href="restaurant.php">
+        <img src="assets/images/image4.png" class="img-fluid img-max img-home-button">
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="photo.php" >
-        <img src="assets/images/image4.jpg" class="img-fluid img-max img-home-button">
+        <a href="contact.php" target="_blank">
+        <img src="assets/images/image4.png" class="img-fluid img-max img-home-button">
         </a>
       </div>
     </div>
   </div>
 </div>
 
-<!-- BG BACK -->
-<div class="jarallax jarallax-about" style="background-image: none;" data-jarallax-original-styles="background-image: url('/assets/images/splashart.jpg');">
+<div class="jarallax jarallax-about" style="background-image: url('/assets/images/splashart3.jpg');">
   <div class="container">
     <div class="row">
       <div class="col-12 col-md-6">
 
         <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AU17cEgzWJg?rel=0" allowfullscreen="">
+          <iframe class="embed-responsive-item"
+            src="https://www.youtube.com/watch?v=kJgdCRq-764"
+            allowfullscreen>
           </iframe>
         </div>
               <br>
 
         <div class="embed-responsive embed-responsive-16by9">
-          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rIUJvpotY3s?rel=0" allowfullscreen="">
+          <iframe class="embed-responsive-item"
+            src="https://www.youtube.com/watch?v=kJgdCRq-764"
+            allowfullscreen>
           </iframe>
         </div>
             </div>
@@ -74,7 +75,7 @@
           <tr>
             <td class="align-middle">
               <h1 style="font-weight:600;">ABOUT THE GUILD</h1>
-              <p>Snow Crows is a community of hardcore PvE players who strive to be at the top in terms of raids, fractals and dungeons. Outside of weekly raiding and daily fractals, record attempts are likely to take place for the most engaged players. Additionally, we provide builds, guides &amp; benchmarks for the community to use.</p>
+              <p>Snow Crows is a community of hardcore PvE players who strive to be at the top in terms of raids, fractals and dungeons. Outside of weekly raiding and daily fractals, record attempts are likely to take place for the most engaged players. Additionally, we provide builds, guides & benchmarks for the community to use.</p>
 
             </td>
           </tr>
@@ -83,32 +84,27 @@
       </div>
     </div>
   </div>
-<div id="jarallax-container-1" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; z-index: -100;"><div style="background-position: 50% 50%; background-size: cover; background-repeat: no-repeat; background-image: url(&quot;/assets/images/splashart.jpg&quot;); position: fixed; top: 0px; left: 0px; width: 819px; height: 569.125px; overflow: hidden; pointer-events: none; margin-top: 22.9375px; transform: translate3d(0px, 168.5px, 0px);">
-
-</div>
-</div>
 </div>
 
-<!-- CONTAINER DARK HIDDEN -->
-
-<!-- <div class="container-fluid container-dark hidden" style="border-bottom: 24px solid #181818;padding-bottom:15px;">
+<div class="container-fluid container-dark hidden" style="border-bottom: 24px solid #181818;padding-bottom:15px;">
   <div class="container">
     <div class="row">
       <div class="col-6 col-md-3">
-        <img src="assets/images/imagetest.jpg" class="img-fluid">
+        <img src="/assets/images/imagetest.png" class="img-fluid">
       </div>
       <div class="col-6 col-md-3">
-        <img src="assets/images/image5.jpg" class="img-fluid">
+        <img src="/assets/images/imagetest.png" class="img-fluid">
       </div>
       <div class="col-6 col-md-3">
-        <img src="assets/images/image4.jpg" class="img-fluid">
+        <img src="/assets/images/imagetest.png" class="img-fluid">
       </div>
       <div class="col-6 col-md-3">
-        <img src="assets/images/image4.jpg" class="img-fluid">
+        <img src="/assets/images/imagetest.png" class="img-fluid">
       </div>
     </div>
   </div>
-</div> -->
+</div>
+
 </main>
     <!-- <div class="container-fluid">
 
