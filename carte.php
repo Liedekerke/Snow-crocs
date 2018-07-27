448aa731a901c8d8e6dd041e3fc4918a49fb1a0a
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-27 13:18:07 +0200

    fin

--- carte.php
+++ carte.php
@@ -13,27 +13,46 @@
 
 <!-- MAIN -->
 
-  <section class="cartouchette">
-    <div id="entree">
+  <!-- <section class="cartouchette"> -->
+    <div class="row" id="entree">
       <h2 class="soustitre">Entrée</h2>
-      <p>Tomate Crevettes Grises – revisitée – // Millefeuille / Persil / Sablé <span id="bad1" class="badge"> 400€</span></a><br> Foie Gras de Canard – mi-cuit – // Gelée Gingembre / Chutney Airelles / Brioche<span id="bad2" class="badge">401€</span></a><br></p>
+      <p class="col-sm-10 col-md-10 col-lg-10">Tomate Crevettes Grises – revisitée – // Millefeuille / Persil / Sablé
+         <span id="bad1" class="badge col-sm-2 col-md-2 col-lg-2"> 400€</span>
+       </a><br>
+        Foie Gras de Canard – mi-cuit – // Gelée Gingembre / Chutney Airelles / Brioche
+        <span id="bad2" class="badge col-sm-2 col-md-2 col-lg-2">401€</span>
+      </a><br>
+    </p>
     </div>
-    <div id="plat">
+
+    <div class="row"  id="plat">
       <h2 class="soustitre">plat</h2>
-      <p>Solette – façon Ostendaise – // Crevettes Grises / Epinards Frais <span id="bad3" class="badge">600€</span></a><br> Chicon au Gratin – revisité – // Fruits Secs / Crème de Vieux Bruges <span id="bad4" class="badge">666€</span></a><br> Bavette de
-        Bœuf – Irish Hereford – // Jus Corsé / Béarnaise / Gros Légumes <span id="bad5" class="badge">669€</span></a>
+      <p class="col-sm-10 col-md-10 col-lg-10">Solette – façon Ostendaise – // Crevettes Grises / Epinards Frais
+        <span id="bad3" class="badge col-sm-2 col-md-2 col-lg-2">600€</span>
+      </a><br>
+       Chicon au Gratin – revisité – // Fruits Secs / Crème de Vieux Bruges
+       <span id="bad4" class="badge col-sm-2 col-md-2 col-lg-2">666€</span>
+     </a><br>
+      Bavette de
+        Bœuf – Irish Hereford – // Jus Corsé / Béarnaise / Gros Légumes
+         <span id="bad5" class="badge col-sm-2 col-md-2 col-lg-2">669€</span>
+       </a>
         </br>
       </p>
-
     </div>
-    <div id="dessert">
+
+    <div class="row"  id="dessert">
       <h2 class="soustitre">dessert</h2>
-      <p>La Merveilleuse Boule Surprise // Chocolat / Bergamote / Calamansi / Meringue <span id="bad6" class="badge">898€</span></a><br> Crémeux Chocolat Noir – free gluten & lactose – // Confit Orange / Streusel Noisette <span id="bad7" class="badge">900€</span></a>
+      <p class="col-sm-10 col-md-10 col-lg-10">La Merveilleuse Boule Surprise // Chocolat / Bergamote / Calamansi / Meringue
+        <span id="bad6" class="badge col-sm-2 col-md-2 col-lg-2">898€</span>
+      </a><br>
+       Crémeux Chocolat Noir – free gluten & lactose – // Confit Orange / Streusel Noisette
+       <span id="bad7" class="badge col-sm-2 col-md-2 col-lg-2">900€</span></a>
         </br>
       </p>
     </div>
 
-  </section>
+  <!-- </section> -->
 
 <!-- FOOTER -->
 
