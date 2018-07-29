48321259b7000b1a4ac45bd5234dc8e984bb5511
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-29 02:36:24 +0200

    fin

--- contact.php
+++ contact.php
@@ -6,35 +6,41 @@
    <?php include('assets/includes/header.php'); ?>
    <!-- MAIN -->
 
-  <section class="padd">
-    <div id="contact">
-      <form action="" method="post" id="form">
-        <div>
-          <label class="name">  <span class="glyphicon glyphicon-user"></span>Firstname</label><input type="text" name="Firstname">
-        </div>
-        <div>
-          <label class="name">  <span class="glyphicon glyphicon-user"></span>Lastname</label><input type="text" name="Lastname">
-        </div>
-        <div>
-          <label class="name"><span class="glyphicon glyphicon-envelope"></span> e-mail</label><input type="text" name="e-mail">
-        </div>
-        <div class="marge">
-          <select>
-                      <option value="we">WEEEEEEEEEEE</option>
-                      <option value="woe">WOEEEEEEEEE</option>
-                      <option value="wa">WAAAAAAAAAAA</option>
-                      <option value="wo">WOOOOOOOOOO</option>
-                    </select>
-        </div>
-        <div>
-          <label class="name"> <span class="glyphicon glyphicon-pencil"></span> Message</label>
-          <textarea name="message"></textarea>
-          <button type="submit">Send</button>
-        </div>
-      </form>
+   <section id="contact" class="content-section text-center">
+         <div class="contact-section">
+             <div class="container">
+               <h2>Contact Us</h2>
+               <p>Feel free to shout us by feeling the contact form or visiting our social network sites like Fackebook,Whatsapp,Twitter.</p>
+               <div class="row">
+                 <div class="col-md-8 col-md-offset-2">
+                   <form class="form-horizontal">
+                     <div class="form-group">
+                       <label for="exampleInputName2">Name</label>
+                       <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+                     </div>
+                     <div class="form-group">
+                       <label for="exampleInputEmail2">Email</label>
+                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+                     </div>
+                     <div class="form-group ">
+                       <label for="exampleInputText">Your Message</label>
+                      <textarea  class="form-control" placeholder="Description"></textarea>
+                     </div>
+                     <button type="submit" class="btn btn-default">Send Message</button>
+                   </form>
 
-    </div>
-  </section>
+                   <hr>
+                     <h3>Our Social Sites</h3>
+                   <ul class="list-inline banner-social-buttons">
+                     <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-twitter"> <span class="network-name">Twitter</span></i></a></li>
+                     <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-facebook"> <span class="network-name">Facebook</span></i></a></li>
+                     <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-youtube-play"> <span class="network-name">Youtube</span></i></a></li>
+                   </ul>
+                 </div>
+               </div>
+             </div>
+         </div>
+       </section>
 
 
 
