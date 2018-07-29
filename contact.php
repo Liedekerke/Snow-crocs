7cd9990739ad18453adfc58f0002c79b6bf5a691
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-29 02:21:30 +0200

    fin

--- contact.php
+++ contact.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html>
-
-<head>
 <?php include('assets/includes/head.php'); ?>
-  <title>Va & Vins</title>
+<title>photo</title>
 </head>
-
-<body style="background:black !important">
-
-
-
-<?php include('assets/includes/header.php'); ?>
-
-
+<body style="background-color:#1e2226b8 !important;">
+   <!-- HEADER -->
+   <?php include('assets/includes/header.php'); ?>
+   <!-- MAIN -->
 
   <section class="padd">
     <div id="contact">
@@ -46,28 +38,5 @@
 
 
 
-
-
-<?php include('assets/includes/footer.php'); ?>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  <!-- <script>
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
-        });
-      });
-    }
-  </script> -->
-</body>
-
-</html>
+     <!-- FOOTER -->
+     <?php include('assets/includes/footer.php'); ?>
--- restaurant.php
+++ restaurant.php
@@ -1,26 +1,17 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <?php include('assets/includes/head.php'); ?>
-  <title>Va & Vins</title>
+<?php include('assets/includes/head.php'); ?>
+<title>photo</title>
 </head>
-
-<body style="background:black !important">
-
-
-
-<?php include('assets/includes/header.php'); ?>
+<body style="background-color:#1e2226b8 !important;">
+   <!-- HEADER -->
+   <?php include('assets/includes/header.php'); ?>
+   <!-- MAIN -->
 
 
 
-  <!-- <section class="container justify-content-center"> -->
-  <section class="row">
+    <div id="resto1" class="col-lg-6 col-sm-6 col-md-6">
 
-    <div id="resto1" class="col-lg-6">
-      <!-- <div id="resto1" class="col-lg-4 col⁻lg-offset-2"> -->
-      <h1>Va & Vins</h1>
-      <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2519.233764731581!2d4.3550966!3d50.8453553!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sbe!4v1527514264246" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></br></iframe></br> -->
+      <h1>Snow Crocs</h1>
+
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.2337658368315!2d4.355096615745739!3d50.845355279531816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c47fce0ba71d%3A0x2f634768a374d3b0!2sCantersteen+10%2C+1000+Bruxelles!5e0!3m2!1sfr!2sbe!4v1527539858581"
         width="50%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
       <p>adress: Cantersteen 10, Bruxelles</br>
@@ -28,10 +19,10 @@
       <p> H24 7/7</p>
     </div>
 
-    <div id="resto2" class="col-lg-6">
-      <!-- <div id="resto2" class="col-lg-4 col-lg-offset-1"> -->
-      <h1>Va & Vins</h1>
-      <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2519.233764731581!2d4.3550966!3d50.8453553!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sbe!4v1527514264246" width="300" height="300" frameborder="0" style="border:0" allowfullscreen></br></iframe></br> -->
+    <div id="resto2" class="col-lg-6 col-sm-6 col-md-6">
+
+      <h1>Snow Crocs</h1>
+
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.2337658368315!2d4.355096615745739!3d50.845355279531816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c47fce0ba71d%3A0x2f634768a374d3b0!2sCantersteen+10%2C+1000+Bruxelles!5e0!3m2!1sfr!2sbe!4v1527539858581"
         width="50%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
       <p>adress: Cantersteen 10, Bruxelles</br>
@@ -40,30 +31,9 @@
     </div>
 
 
-  </section>
 
 
 
-<?php include('assets/includes/footer.php'); ?>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  <!-- <script>
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-          // Registration was successful
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-          // registration failed :(
-          console.log('ServiceWorker registration failed: ', err);
-        });
-      });
-    }
-  </script> -->
-</body>
-
-</html>
+       <!-- FOOTER -->
+       <?php include('assets/includes/footer.php'); ?>
