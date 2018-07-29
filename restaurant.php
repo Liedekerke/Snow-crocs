9382506f4461c324818d4512fe5e62767ac86913
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-29 02:30:34 +0200

    fin

--- restaurant.php
+++ restaurant.php
@@ -13,7 +13,7 @@
       <h1>Snow Crocs</h1>
 
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.2337658368315!2d4.355096615745739!3d50.845355279531816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c47fce0ba71d%3A0x2f634768a374d3b0!2sCantersteen+10%2C+1000+Bruxelles!5e0!3m2!1sfr!2sbe!4v1527539858581"
-        width="50%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        width="70%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
       <p>adress: Cantersteen 10, Bruxelles</br>
       </p>
       <p> H24 7/7</p>
@@ -24,7 +24,7 @@
       <h1>Snow Crocs</h1>
 
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.2337658368315!2d4.355096615745739!3d50.845355279531816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c47fce0ba71d%3A0x2f634768a374d3b0!2sCantersteen+10%2C+1000+Bruxelles!5e0!3m2!1sfr!2sbe!4v1527539858581"
-        width="50%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        width="70%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
       <p>adress: Cantersteen 10, Bruxelles</br>
       </p>
       <p> H24 7/7</p>
