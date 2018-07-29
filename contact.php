963b3bcd7886bf92c5ac59dd459d7e90dc516703
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-07-29 02:57:53 +0200

    fin

--- contact.php
+++ contact.php
@@ -1,43 +1,9 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-
-<meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700" rel="stylesheet">
-
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"> -->
-
-<link rel="stylesheet" href="/assets/css/jarallax.css">
-<script src="assets/js/jarallax.js"></script>
-<!-- <script src="assets/js/jarallax-video.js"></script> -->
-<script src="assets/js/jarallax-element.js"></script>
-
-<!-- <script src="assets/js/jquery-3.3.1.min.js"></script> -->
-
-<link rel="stylesheet" href="assets/css/style.css">
-
-
+<?php include('assets/includes/head.php'); ?>
+<title>Contact</title>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-
-
-
-<link rel="manifest" href="/assets/manifest.json" />
-
-<meta name="theme-color" content="#317EFB"/>
-<meta name="Description" content="oui"/>
-<link href="assets/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-
-<title>Contact</title>
 </head>
-
 <body style="background-color:#1e2226b8 !important;">
    <!-- HEADER -->
    <?php include('assets/includes/header.php'); ?>
