ab86381bc43f9b129abc33cf9ba8b40e1656383c
Nicolas de Liedekerke <38915473+Liedekerke@users.noreply.github.com>
2018-10-11 17:34:45 +0200

    grind test

--- carte.php
+++ carte.php
@@ -2,7 +2,7 @@
 <title>carte</title>
 </head>
 <!-- <body style="background-color:#1e2226b8 !important;"> -->
-<body id="bg-menu" style="background-image: url('assets/images/imgtest/bg-menus.jpg' );">
+<body id="bg-menu" class="grid-container" style="background-image: url('assets/images/imgtest/bg-menus.jpg' );">
 
    <!-- HEADER -->
    <?php include('assets/includes/header.php'); ?>
@@ -53,6 +53,40 @@
    </div>
  </div>
    </div>
-</body>
+
    <!-- FOOTER -->
-   <?php include('assets/includes/footer.php'); ?>
+
+     <footer>
+     <div class="main-footer">
+       <p id="copiriri"> <span class="glyphicon glyphicon-copyright-mark"></span> Copyright- Nicolas de Liedekerke </p>
+     </div>
+   </footer>
+
+   <script type="text/javascript">
+       jarallax(document.querySelectorAll('.jarallax'));
+       jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+           keepImg: true,
+       });
+   </script>
+
+
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
+
+
+   <script>
+     if ('serviceWorker' in navigator) {
+       window.addEventListener('load', function() {
+         navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+           // Registration was successful
+           console.log('ServiceWorker registration successful with scope: ', registration.scope);
+         }, function(err) {
+           // registration failed :(
+           console.log('ServiceWorker registration failed: ', err);
+         });
+       });
+     }
+   </script>
+
+   </body>
+   </html>
